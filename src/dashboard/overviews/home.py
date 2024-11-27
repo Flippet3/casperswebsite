54356bd753dc5b38b917aa7494ff36fff594aa44
Flippet3 <casperbekkers2@hotmail.com>
2024-11-27 21:33:57 +0100

    Minor home change

--- src/dashboard/overviews/home.py
+++ src/dashboard/overviews/home.py
@@ -11,7 +11,7 @@ class Home(OverviewBase):
     def app_content(cls, bootstrap: CustomTemplate) -> CustomTemplate:
         bootstrap.add_card("Intro")
         bootstrap.add_container(6)
-        bootstrap.add_text("Hello -- welcome to my website! My name is Casper and I'm a Dutch guy living in Denmark. For those interested in how I made this, check out the 'how it's made' page. For those interested in why I made this, I can't help you. It just kind of happened.")
+        bootstrap.add_text("Hello -- welcome to my website! My name is Casper and I'm a Dutch guy living in Denmark. For those interested in how I made this website, check out the 'how it's made' page. For those interested in why I made this website, I can't help you -- it just kind of happened.")
         bootstrap.add_container(6)
         bootstrap.add_image("https://s7.gifyu.com/images/SGR3S.gif")
         return bootstrap
