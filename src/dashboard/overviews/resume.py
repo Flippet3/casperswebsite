f3d0abcb698499042c85cf0d3268d18d0c8a0dc6
Flippet3 <casperbekkers2@hotmail.com>
2024-11-27 13:52:51 +0100

    Added main two jobs to resume

--- src/dashboard/overviews/resume.py
+++ src/dashboard/overviews/resume.py
@@ -10,11 +10,21 @@ from general_tools.general_tools import get_base_folder
 
 class Resume(OverviewBase):
     overview_category = OverViewCategory.CV
+    # Image source: https://icons8.com/icon/set/graduate/material
 
     @classmethod
     def app_content(cls, bootstrap: CustomTemplate) -> CustomTemplate:
         def add_education_card(school: str, education_type: str, start_year: int, end_year: int, img_ref: str, text: str, skills: list[str]):
-            bootstrap.add_card(f"({start_year} - {end_year}) Education - {education_type} - {school}", skills=skills)
+            bootstrap.add_card(f"({start_year} - {end_year}) <img src='https://img.icons8.com/?size=24&id=86200&format=png'> {education_type} - {school}", skills=skills)
+            bootstrap.add_container(10)
+            bootstrap.add_text(text)
+            bootstrap.add_container(2)
+            bootstrap.add_image(img_ref)
+            bootstrap.add_container(12)
+            bootstrap.add_text(f"Skills: {', '.join(skills)}")
+
+        def add_job_card(company: str, role: str, start_year: int, end_year: int, img_ref: str, text: str, skills: list[str]):
+            bootstrap.add_card(f"({start_year} - {end_year}) <img src='https://img.icons8.com/?size=24&id=xkZW9nXosg6X&format=png'> {company} - {role}", skills=skills)
             bootstrap.add_container(10)
             bootstrap.add_text(text)
             bootstrap.add_container(2)
@@ -56,5 +66,11 @@ class Resume(OverviewBase):
         education_cards = sorted(filter(lambda x: x["type"] == "education", cards), key=lambda x: x["start"], reverse=True)
         for card in education_cards:
             add_education_card(card["school"], card["education_type"], card["start"].year, card["end"].year, card["img_ref"], card["text"], card["skills"])
+
+        job_cards = sorted(filter(lambda x: x["type"] == "job", cards), key=lambda x: x["start"],
+                                 reverse=True)
+        for card in job_cards:
+            add_job_card(card["company"], card["role"], card["start"].year, card["end"].year,
+                               card["img_ref"], card["text"], card["skills"])
 
         return bootstrap
